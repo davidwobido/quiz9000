778f434225ea1938783d01c0ff28c1aacca95854
davidwobido <davidwobido@googlemail.com>
2021-10-11 17:20:56 +0200

    Add if else statement

--- quiz9000/main.js
+++ quiz9000/main.js
@@ -1,2 +1,26 @@
+const correctAnswer = true;
+
 const Question = document.querySelector(".question");
-Question.textContent = "This is the first question";
+Question.textContent = "Is this the first question?";
+
+const yesButton = document.querySelector(".yes");
+yesButton.onclick = function () {
+  if (correctAnswer === true) {
+    alert("The answer is right! 🙃");
+  } else {
+    alert("This answer is wrong☹️");
+  }
+};
+
+// const noButton = document.querySelector(".no");
+// noButton.onclick = function () {
+//   alert("The answer is wrong! :(");
+// };
+
+//   document.querySelector(".yes").onclick = function () {
+//     alert("The answer is right! 🙃");
+//   };
+
+//   document.querySelector(".no").onclick = function () {
+//     alert("The answer is wrong! ☹️");
+//   };
