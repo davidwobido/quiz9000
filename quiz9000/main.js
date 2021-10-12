24b25133800d69f477b1af63d0140a63d2486b8c
davidwobido <davidwobido@googlemail.com>
2021-10-12 12:12:22 +0200

    Show text if answer is correct or incorrect

--- quiz9000/main.js
+++ quiz9000/main.js
@@ -6,11 +6,58 @@ Question.textContent = "Is this the first question?";
 const yesButton = document.querySelector(".yes");
 yesButton.onclick = function () {
   if (correctAnswer === true) {
-    alert("The answer is right! 🙃");
+    // Variable resultElement anlegen, die ein p erstellt
+    const resultElement = document.createElement("p");
+    // p gibt Text aus
+    resultElement.textContent = "That’s right!";
+    // p Klasse geben
+    resultElement.className = "correct";
+    //resultElement dem body als letztes hinzufügen
+    document.body.append(resultElement);
   } else {
-    alert("This answer is wrong☹️");
+    alert("Try again");
   }
 };
+
+const noButton = document.querySelector(".no");
+noButton.onclick = function () {
+  if (correctAnswer === true) {
+    // Variable resultElement anlegen, die ein p erstellt
+    const resultElement = document.createElement("p");
+    // p gibt Text aus
+    resultElement.textContent = "That’s wrong";
+    // p Klasse geben
+    resultElement.className = "incorrect";
+    //resultElement dem body als letztes hinzufügen
+    document.body.append(resultElement);
+  } else {
+    alert("Try again");
+  }
+};
+
+//solution 1
+// const yesButton = document.querySelector(".yes");
+
+// yesButton.onclick = function () {
+//   if (correctAnswer === true) {
+//     const correct = document.createElement("p");
+//     correct.className = "testclass";
+//     const correctAnswerText = document.createTextNode("Your answer is correct");
+//     correct.appendChild(correctAnswerText);
+//     document.body.appendChild(correctAnswerText);
+//   } else {
+//     alert("Try again");
+//   }
+// };
+
+// const yesButton = document.querySelector(".yes");
+// yesButton.onclick = function () {
+//   if (correctAnswer === true) {
+//     alert("The answer is right! 🙃");
+//   } else {
+//     alert("This answer is wrong☹️");
+//   }
+// };
 
 // const noButton = document.querySelector(".no");
 // noButton.onclick = function () {
