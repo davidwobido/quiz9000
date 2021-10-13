2b1ae6db987ccb1025ba56c9a9ca3d70d113fa0b
davidwobido <davidwobido@googlemail.com>
2021-10-13 20:43:07 +0200

    Add Arrays, Objects and a Start again Button

--- quiz9000/main.js
+++ quiz9000/main.js
@@ -1,41 +1,43 @@
-const questionList = [
-  "Is this a quiz?",
-  "Is the name of the quiz 'Quiz 9001'?",
-  "Is the backgound blue?",
-  "Is the font color pink?",
-  "Do you still enjoy this quiz?",
+const questions = [
+  { question: "Is this a quiz?", answer: true },
+  { question: "Is the name of the quiz 'Quiz 9001'?", answer: false },
+  { question: "Is the background blue?", answer: true },
+  { question: "Is the font color pink?", answer: false },
+  { question: "Do you still enjoy this quiz?", answer: true },
 ];
 
-const answerList = [true, false, true, false, true];
 let questionIndex = 0;
-let correctAnswer = answerList[questionIndex];
+let correctAnswer = questions[questionIndex].answer;
 
-//Question counter
+// Question counter
 function updateProgress() {
   const progressElement = document.querySelector(".progress");
   progressElement.textContent = `Question ${questionIndex + 1}/${
-    questionList.length
+    questions.length
   }`;
   // same as: "Question " + (questionIndex + 1) + "/" + questionList.length;
 }
 
 // Question
-const question = document.querySelector(".question");
-question.textContent = questionList[questionIndex];
+const myQuestion = document.querySelector(".question");
+myQuestion.textContent = questions[questionIndex].question;
 updateProgress();
 
+// Finishing the quiz
 function finishQuiz() {
   const finishElement = document.querySelector(".quizcard");
   finishElement.textContent = "Finished 🏁";
   finishElement.className = "finishElement";
 }
 
+// Jump to next Question
 function nextQuestion() {
-  questionIndex += 1;
-  Print.removeChild(document.querySelector(".correct"));
-  if (questionIndex < questionList.length) {
-    correctAnswer = answerList[questionIndex];
-    question.textContent = questionList[questionIndex];
+  questionIndex += 1; //qI steigt
+  Print.removeChild(document.querySelector(".correct")); // Correct verschwindet
+  if (questionIndex < questions.length) {
+    //qI kleiner als gesamt
+    correctAnswer = questions[questionIndex].answer; //dann richtige Antwort true oder false
+    myQuestion.textContent = questions[questionIndex].question; //textinhalt je nach qI
     updateProgress();
   } else {
     finishQuiz();
@@ -62,25 +64,44 @@ noButton.onclick = function () {
   }
 };
 
+// Building an element:
+function createMyElement(tagName, properties) {
+  const element = document.createElement(tagName);
+  element.className = properties.className;
+  element.textContent = properties.textContent;
+  return element;
+}
+
 function showAnswerIsCorrect() {
-  // Variable resultElement anlegen, die ein p erstellt
-  const resultElement = document.createElement("p");
-  // p hat den Inhalt
-  resultElement.textContent = "That’s right! 🙃";
-  // p hat die Klasse
-  resultElement.className = "correct";
-  // resultElement dem body hinzufügen (append am Ende. prepend am Anfang.)
-  Print.appendChild(resultElement);
-  // disableButtons();
+  const p = createMyElement("p", {
+    className: "correct",
+    textContent: "That’s right! 🙃",
+  });
+  Print.appendChild(p);
   setTimeout(nextQuestion, 800);
 }
 
 function showAnswerIsIncorrect() {
-  const resultElement = document.createElement("p");
-  resultElement.textContent = "That’s wrong 🙁";
-  resultElement.className = "incorrect";
-  Print.appendChild(resultElement);
+  const p = createMyElement("p", {
+    className: "incorrect",
+    textContent: "That’s wrong 🙁",
+  });
+  Print.appendChild(p);
+  setTimeout(nextQuestion, 800);
 }
+
+const startAgain = document.querySelector(".startAgain");
+startAgain.textContent = "Start again";
+startAgain.onclick = function () {
+  questionIndex = 0;
+  myQuestion.textContent = questions[questionIndex].question;
+  correctAnswer = questions[questionIndex].answer;
+  updateProgress();
+};
+
+// function begin() {
+//   questionIndex = 0;
+// }
 
 // const nextButton = document.createElement("p");
 // nextButton.textContent = "Next Question";
@@ -92,3 +113,23 @@ function showAnswerIsIncorrect() {
 //   // Print.removeChild(document.querySelector(".correct"));
 //   // .querySelector(".quizcard")
 //   // .removeChild(document.querySelector(".print"));
+
+// function showAnswerIsIncorrect() {
+//   const resultElement = document.createElement("p");
+//   resultElement.textContent = "That’s wrong 🙁";
+//   resultElement.className = "incorrect";
+//   Print.appendChild(resultElement);
+// }
+
+// function showAnswerIsCorrect() {
+//   // Variable resultElement anlegen, die ein p erstellt
+//   const resultElement = document.createElement("p");
+//   // p hat den Inhalt
+//   resultElement.textContent = "That’s right! 🙃";
+//   // p hat die Klasse
+//   resultElement.className = "correct";
+//   // resultElement dem body hinzufügen (append am Ende. prepend am Anfang.)
+//   Print.appendChild(resultElement);
+//   // disableButtons();
+//   setTimeout(nextQuestion, 800);
+// }
